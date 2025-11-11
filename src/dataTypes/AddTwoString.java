9a7ceb84c84d6c10f16f5fa98e415f79ebb3c03d
Sathappan89 <sathappanas1989@gmail.com>
2025-11-10 19:24:55 -0700

    NAL_Java_Execrise

---
+++ src/dataTypes/AddTwoString.java
@@ -0,0 +1,19 @@
+package dataTypes;
+
+public class AddTwoString {
+
+	/*
+	 * Write a Java Program to add two strings String a = "Hello"; String b =
+	 * "Sathappan Annamalai"
+	 */
+
+	public static void main(String[] args) {
+
+		String a = "Hello";
+		String b = "Sathappan Annamalai";
+
+		System.out.println(a + " " + b);
+
+	}
+
+}
