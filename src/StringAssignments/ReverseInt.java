075827c1901e01fa9826191658e7f53516f53720
Sathappan89 <sathappanas1989@gmail.com>
2025-12-11 04:53:30 -0700

    ReverseInt

---
+++ src/StringAssignments/ReverseInt.java
@@ -0,0 +1,34 @@
+package StringAssignments;
+
+public class ReverseInt {
+
+	public static int reverse(int num) {
+
+		// convert number to string
+		String s = String.valueOf(num);
+		String rev = "";
+
+		// reverse using for-loop
+		for (int i = s.length() - 1; i >= 0; i--) {
+			rev = rev + s.charAt(i);
+		}
+
+		// convert back to int
+		return Integer.parseInt(rev);
+	}
+
+	public static void main(String[] args) {
+		int[] input = { 56789, 12345 };
+
+		for (int i = 0; i < input.length; i++) {
+			input[i] = reverse(input[i]);
+		}
+
+		// print results
+		for (int num : input) {
+			System.out.print(num + " ");
+		}
+
+	}
+
+}
