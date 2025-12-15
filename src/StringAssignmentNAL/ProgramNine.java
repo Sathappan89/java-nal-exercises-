f3022b7ccdfdd1f3452fd01c74567aee38922d87
Sathappan89 <sathappanas1989@gmail.com>
2025-12-14 21:27:48 -0700

    String Assignments

---
+++ src/StringAssignmentNAL/ProgramNine.java
@@ -0,0 +1,24 @@
+package StringAssignmentNAL;
+/*Write a program that breaks a whole string into small strings, and prints out its all values . (Hint: split,
+loop) .*/
+public class ProgramNine {
+	
+	public static void result(String a) {
+		
+		String results = " ";
+		
+		String sp[]=a.split(" ");
+		
+		for(int i=0;i<sp.length;i++) {
+			System.out.println(sp[i]);
+		}
+		
+	
+	}
+
+	public static void main(String[] args) {
+		String a = "My Name is Sathappan";
+		result(a);
+	}
+
+}
